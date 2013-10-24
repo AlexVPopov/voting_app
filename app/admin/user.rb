a1c8a5fb7e1b0f5faee4882c23678bfed6bda00e
Alex Popov <alex.val.popov@gmail.com>
2013-10-24 15:53:07 +0300

    Customize user resource and modify devise initializer

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register User do
+  index do
+    column :username
+    default_actions
+  end
 
+  filter :username
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :username
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit admin_user: [:username, :password, :password_confirmation]
+    end
+  end
 end
